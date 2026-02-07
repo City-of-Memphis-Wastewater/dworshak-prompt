03f4f5ce5d181d79294ec3634d703ba664b99810
City of Memphis Wastewater <george.bennett@memphistn.gov>
2026-02-06 23:18:24 -0600

    protect __init__

--- src/dworshak_prompt/__init__.py
+++ src/dworshak_prompt/__init__.py
@@ -1,5 +1,15 @@
+"""
 from .multiplexer import DworshakPrompt
 
 all = [
     "DworshakPrompt"
 ]
+"""
+
+__all__ = ["DworshakPrompt"]
+
+def __getattr__(name):
+    if name == "DworshakPrompt":
+        from .multiplexer import DworshakPrompt
+        return DworshakPrompt
+    raise AttributeError(name)
