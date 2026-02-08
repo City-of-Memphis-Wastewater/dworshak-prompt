b5a02698a0c91c6dc8271a52b56e6ef2ab7b45f2
City of Memphis Wastewater <george.bennett@memphistn.gov>
2026-02-08 10:52:48 -0600

    cli.py mvp

--- src/dworshak_prompt/cli.py
+++ src/dworshak_prompt/cli.py
@@ -1,3 +1,24 @@
 # src/dworshak_prompt/cli.py
 
 import typer
+from dworshak_prompt import DworshakPrompt, PromptMode
+import os
+
+app = typer.Typer()
+
+@app.command()
+def ask(
+    mode: str = "console",
+    env: str | None = None,
+):
+    val = DworshakPrompt.ask(
+        "Enter value:",
+        priority=[PromptMode(mode)],
+        debug=True,
+    )
+    if env and val:
+        print(f"export {env}={val}")
+        os.environ[env] = val
+
+if __name__ == "__main__":
+    app()
