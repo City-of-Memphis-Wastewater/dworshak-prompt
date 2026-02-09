a109550ec40686e853b68085534f8a3634fc0d69
George Bennett (oolong-wsl2) <george.bennett@memphistn.gov>
2026-02-09 12:15:27 -0600

    reduce height of gui popup

--- src/dworshak_prompt/gui_prompt.py
+++ src/dworshak_prompt/gui_prompt.py
@@ -19,7 +19,7 @@ class CustomPromptDialog:
         
         # Set a minimum width and padding
         # We target ~400px width to ensure the title isn't truncated
-        min_w, min_h = 400, 180
+        min_w, min_h = 400, 100
         screen_w = parent.winfo_screenwidth()
         screen_h = parent.winfo_screenheight()
         
