ae76a5fb01987b1ffe17aea67c7163169fb5d856
George Bennett (oolong-wsl2) <george.bennett@memphistn.gov>
2026-05-08 03:00:39 -0500

    suppress cli_stdlib. console_stdlib lives on.

--- src/dworshak_prompt/__main__.py
+++ src/dworshak_prompt/__main__.py
@@ -1,13 +1,21 @@
 # src/dworshak_prompt/__main__.py
+import sys
+TYPERSUCCESS=NONE
 try:
+    import typer
+    TYPERSUCCESS=True
+except (ImportError, ModuleNotFoundError):
+    # Fallback to the 'lifeboat' CLI
+    #from .cli_stdlib import main as run
+    TYPERSUCCESS=FALSE
+ 
+if TYPERSUCCESS:
+    # Attempt to use the feature-rich CLI
     from .cli import app
     def run():
         app()
-except ImportError:
-    #from .cli_stdlib import main as run
+else:
     import sys
-    #print("Please install this package with the 'typer' extra to utilize the CLI.", file=sys.stderr)
-    # Fallback when dependencies are missing
     def run():
         print(
             "Please install this package with the 'typer' extra to utilize the CLI.",
