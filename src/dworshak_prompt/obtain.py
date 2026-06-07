ed665489e58ce8ef4047155e176f10c409d3ed20
City of Memphis Wastewater <george.bennett@memphistn.gov>
2026-06-06 19:03:28 -0500

    get rid of force_prompt which is baaicallt redundant for overwrite. tag 0.3.10.4

--- src/dworshak_prompt/obtain.py
+++ src/dworshak_prompt/obtain.py
@@ -67,7 +67,7 @@ class Obtain:
         self.interface_priority = interface_priority
         self.interface_avoid = interface_avoid
         self.interrupt_behavior = interrupt_behavior
-        self.force_prompt = force_prompt
+        #self.force_prompt = force_prompt
         self.prompt = DworshakPrompt(
             interface_priority=interface_priority, # instantiated value can be overrode for each function call
             interface_avoid=interface_avoid, # instantiated value can be overrode for each function call 
